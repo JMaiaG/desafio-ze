a4680dba7e4f255b0f82f4488acc07187bc972f6
JMaiaG <josevictormaia99@outlook.com>
2024-08-26 23:06:36 -0300

    rd commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-let nome = "felipe";
+let nomeHeroi = "zézão homelander";
+let xp =
+let ranques = [
+{nome: "Ferro", minXp: 0},
+{nome: "Bronze", minXp: 1000},
+{nome: "Prata", minXp: 2000}
+{nome: "Ouro", minXp: 3000}
+]
+
+if (XP < 1000){
+heroi = "Ferro";
+}
 console.log(nome);
